6f016ce0a73d300cc16211aace35e22ce274a243
mingziday <mingziday@126.com>
2023-12-08 00:12:52 +0800

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import 'semantic-ui-css/semantic.min.css';
+import { products } from "./seed.js";
+import ReactPlayer from 'react-player';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ProductList />
+    );
 }
+function ProductList() {
+    const productComponents = products.map((product) => (
+        <Product prodInfo={product}    />
+    ));
+    return (
+        <div className='ui unstackable items'>
+            {productComponents}
+        </div>
+    );
+
+}
+function Product({prodInfo})  {
+    return (
+        <div className='item'>
+            <div className='image'>
+                <img src={prodInfo.productImageUrl} />
+            </div>
+            <div className='middle aligned content'>
+                <div className='header'>
+                    <i className='large caret up icon' />
+                    {prodInfo.votes}
+                </div>
+                <div className='description'>
+                    <a href={prodInfo.url}>
+                        {prodInfo.title}
+                    </a>
+                    <p>
+                        {prodInfo.description}
+                    </p>
+                </div>
+                <div className='extra'>
+                    <span>Submitted by:</span>
+                    <img
+                        className='ui avatar image'
+                        src={prodInfo.submitterAvatarUrl}
+                    />
+                    <ReactPlayer url='music/03-joe_hisaishi-kaisan-cocmp3.mp3' width='200px' height='60px'/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 
 export default App;
